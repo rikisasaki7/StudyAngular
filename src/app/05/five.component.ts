e3e9776959509ebaa3e9279536f7bb4dc9dc60ed
unagipretz7 <unagipretz7@gmail.com>
2022-01-25 23:33:48 +0900

    5 pg end

--- src/app/05/five.component.ts
+++ src/app/05/five.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core'
+// import { Http, Headers, RequestOptions} from '@angular/http'
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
+import { validateConfig } from '@angular/router/src/config'
+
 
 @Component({
     selector: "my-app-five",
@@ -77,6 +81,98 @@ export class FiveComponent {
         console.log('現在値：' + this.selectOptGroupItem)
         console.log(this.dataGroup)
 
+    }
+
+    max = 140 // 入力可能な最大長
+    tweet = '' // テキストエリアの初期値
+    count = this.max // 入力可能な残り文字数
+    myStyle = { // 残り文字数のスタイル
+        color: '#00f', fontWeight: 'normal'
+    }
+
+    // テキストエリアの変更を監視
+    selector(){
+        // 残り文字数を反映
+        this.count = this.max - this.tweet.length
+        // 残り文字数に応じてスタイルを変更
+        if(this.count > 10){
+            this.myStyle = {
+                color: '#00f', fontWeight: 'normal'
+            }
+        } else if(this.count > 0){
+            this.myStyle = {
+                color: '#f0f', fontWeight: 'normal'
+            }
+        } else {
+            this.myStyle = {
+                color: '#f00', fontWeight: 'bold'
+            }
+
+        }
+    }
+
+    emails: String[] = []
+
+
+    // // Httpサービスを有効化
+    // constructor(private http: Http){}
+
+    // ファイルを選択したらアップロードを実行
+    upload(list: any){
+        // アップロードファイルがなければ処理を中止
+        if(list.length <= 0){return}
+
+        // アップロードファイルを準備
+        let f = list[0]
+        let data = new FormData()
+        data.append('upFile', f, f.name)
+
+        // サーバーにデータを送信
+        // this.http.post('app/this.upload.php', data)
+        //     // 成功／失敗時にはそれぞれ結果をログに表示
+        //     .subscribe(
+        //         data => console.log(data),
+        //         error => console.log(error)
+        //     ) 
+    }
+
+
+    // 個々の入力要素（初期値と検証ルール）を宣言
+    mail = new FormControl('hoge@example.com', [
+        Validators.required,
+        Validators.email
+    ])
+    passwd = new FormControl('', [
+        Validators.required,
+        Validators.minLength(6)
+    ])
+    name = new FormControl('ななし', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(10)
+    ])
+    memo = new FormControl('メモ', [
+        Validators.maxLength(10)
+    ])
+
+    // FormGroupオブジェクトを生成
+    myGroupForm = this.builder.group({
+        mail: this.mail,
+        passwd: this.passwd,
+        name: this.name,
+        memo: this.memo
+    })
+
+    // FormBuilderオブジェクトを生成
+    constructor(private builder: FormBuilder){}
+
+    // サブミット時に入力値を反映
+    showGroup(){
+        console.log(this.mail.value)
+        console.log(this.passwd.value)
+        console.log(this.name.value)
+        console.log(this.memo.value)
+        console.log(this.myGroupForm.value)
     }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@
 import { NgModule, LOCALE_ID }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent }  from './app.component';
 import { ThreeComponent } from './03/three.component';
@@ -12,7 +13,7 @@ import { WingComponent } from './04/wings/wing.component';
 import { FiveComponent } from './05/five.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
   declarations: [ AppComponent, ThreeComponent, FourComponent, EventComponent, BookComponent, WingComponent, FiveComponent ],
   // bootstrap:    [ AppComponent, FourComponent ],
   // providers: [{ provide: LOCALE_ID, useValue: 'de-DE'}]
